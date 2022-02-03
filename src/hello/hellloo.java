16f46fb5ded506e901ccb128ce0f8655419792f4
amal hayeddine <hayeddineamal5@gmail.com>
2022-02-03 09:13:44 -0800

    MY.GAMES

---
+++ src/hello/hellloo.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.lang.reflect.Proxy;
+
+public class hellloo {
+
+	public static void main(String[] args) {
+		
+		prog x=new Prog (6 ,5);
+		x.sum(x , y);
+		System.out.println(x);
+
+	}
+
+
+	}
+
---
+++ src/prog.java
@@ -0,0 +1,8 @@
+
+public class prog {
+	int a,b ;
+     public int sum(int 5, int 9) {
+    	 return ;
+     }
+
+}
